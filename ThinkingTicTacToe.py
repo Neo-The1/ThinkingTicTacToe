0b483b3819fc5f8d41181e1ea2dfb41e5f4ed7a2
Neo-The1 <rahul270890@gmail.com>
2018-06-09 20:30:48 -0400

    added AI play, MonteCarlo needs work

--- ThinkingTicTacToe.py
+++ ThinkingTicTacToe.py
@@ -1,24 +1,39 @@
 #code to play with AI
 from tttBoard import tttBoard
 from MonteCarlo import MonteCarlo
-board = tttBoard(3)
-board.display()
-while len(board.legalMoves()) > 0 and not(board.winner()):
-    print(board.legalMoves())
-    p_move = int(input("choose from above list of moves"))
-    board.makeMove(p_move)
+pBoard = tttBoard(3)
+tttBoard = tttBoard(3)
+pBoard.display()
+def checkWin(board):
+    if (board.winner()):
+        if(board.winner()==1):
+            print("O wins!")
+            return 1
+        elif(board.winner()==2):
+            print("X wins!")
+            return 1
+        elif(board.winner()==-1):
+            print("It's a Draw!")
+            return 1
+    else:
+        return 0
+        
+while len(pBoard.legalMoves()) > 0 and not(checkWin(pBoard)):
+    print(pBoard.legalMoves())
+    pMove = int(input("choose from above list of moves"))
+    pBoard.makeMove(pMove)
     print('your move...')
-    board.display()
-    ttt = MonteCarlo(board)
-    ttt_move = ttt.getMove()
-    board.makeMove(ttt_move)
+    pBoard.display()
+    if checkWin(pBoard):
+        break
+    board_copy = list(pBoard._board)
+    tttBoard._board = board_copy
+    print('Thinking...')
+    ttt = MonteCarlo(tttBoard)
+    tttMove = ttt.getMove()
+    pBoard.makeMove(tttMove)
     print('Thinking Tic Tac Toe move...')
-    board.display()
-    
-if (board.winner()):
-    if(board.winner()==1):
-        print("O wins!")
-    elif(board.winner()==2):
-        print("X wins!")
-    elif(board.winner()==-1):
-        print("It's a Draw!")
+    pBoard.display()
+    if checkWin(pBoard):
+        break
+

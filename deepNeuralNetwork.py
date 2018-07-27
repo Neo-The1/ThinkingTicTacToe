b15719f9b3531e58f1a105f4b974990dc13bc5f1
Neo-The1 <rahul270890@gmail.com>
2018-07-27 17:02:08 -0400

    keras NN implemented, saving/loading still to be done

--- deepNeuralNetwork.py
+++ deepNeuralNetwork.py
@@ -1,8 +1,9 @@
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 import tensorflow as tf
-import numpy as np
 from tensorflow import keras
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 class dnNetwork():
@@ -14,17 +15,18 @@ class dnNetwork():
             first item in the list refers to the size of input layer.
             last item in the list refers to the size of output layer.
         """
+        #currently the code is only for 2 hidden layers, apart from in and out
         self.saveFile = kwds.get('saveFile')
         self._layerSizes = kwds.get('layers', [])
         self._layer1 = keras.layers.Dense(self._layerSizes[1],activation='relu')
         self._layer2 = keras.layers.Dense(self._layerSizes[2],activation='relu')    
-        self._outLayer = keras.layers.Dense(self._layerSizes[0],activation='softmax')
+        self._outLayer = keras.layers.Dense(self._layerSizes[-1],activation='softmax')
         self._inputs = keras.Input(shape=(self._layerSizes[0],)) #returns placeholder
         x = self._layer1(self._inputs)
         x = self._layer2(x)
         self._outputs = self._outLayer(x)
         self._model = keras.Model(inputs=self._inputs,outputs=self._outputs)
-        self._model.compile(optimizer=tf.train.AdamOptimizer,
+        self._model.compile(optimizer=tf.train.AdamOptimizer(0.001),
                       loss='categorical_crossentropy',
                       metrics=['accuracy'])
 
@@ -41,17 +43,37 @@ class dnNetwork():
         self._model.save_weights('./my_model')
         return None
 
-    def train(self, trainData,trainLabels):
+    def train(self, train_x,train_y):
         """ Train the network using passed training data as numpy array
         """
-        self._model.fit(trainData,trainLabels,batch_size=1,epochs=1)
-    
-    def predict(self,inputState):
+        self._model.fit(train_x,train_y,batch_size=32,epochs = 1)
         return None
+    
+    def predict(self,x):
+        """Predict the output, given input
+        """
+        
+        return self._model.predict(x)
 
-#    def evaluate(self, evalData):
-#        """ evaluate accuracy
-#        """
-#        eval_fn = 
-#        accuracy_score = self._estimator.evaluate(input_fn=self._testInputFn)["accuracy"]
-#        print("\nTest Accuracy: {0:f}%\n".format(accuracy_score*100))
+    def evaluate(self,test_x,test_y):
+        """ evaluate accuracy
+        """
+        self._model.evaluate(test_x,test_y)
+        return None
+        
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+#testing on mnist data
+if __name__ == "__main__":
+    #testing on mnist data
+    mnist = input_data.read_data_sets('MNIST_data')
+    testNetwork = dnNetwork(layers = [784,64,32,10])
+    train_x = mnist.train.images
+    train_y = keras.utils.to_categorical(mnist.train.labels)
+    test_x = mnist.test.images
+    test_y = keras.utils.to_categorical(mnist.test.labels)
+    testNetwork.train(train_x,train_y)
+    testNetwork.evaluate(test_x,test_y)
+    predictions = testNetwork.predict(train_x)
+    print("Label: ",np.argmax(train_y[1]))
+    print("Prediction: ",np.argmax(predictions[1]))
